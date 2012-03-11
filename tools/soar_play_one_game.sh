2bce1d8abe6afe61b113189d3ad33d9058ff4b45
Miller Tinkerhess <miller.tinkerhess@gmail.com>
2012-03-11 19:09:29 -0400

    Changed play game script

--- tools/soar_play_one_game.sh
+++ tools/soar_play_one_game.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-./playgame.py --player_seed 42 --end_wait=1.0 --verbose --log_dir game_logs --turns 100 --map_file maps/maze/maze_04p_01.map --turntime 60000 --html replay.html --nolaunch "$@" "../soar_bot/SoarBot" "python sample_bots/python/LeftyBot.py" "python sample_bots/python/HunterBot.py" "python sample_bots/python/GreedyBot.py"
+./playgame.py --player_seed 42 --end_wait=1.0 --verbose --log_dir game_logs --turns 30 --map_file maps/maze/maze_04p_01.map --turntime 60000 --html replay.html --nolaunch "$@" "../soar_bot/SoarBot" "python sample_bots/python/LeftyBot.py" "python sample_bots/python/HunterBot.py" "python sample_bots/python/GreedyBot.py"
